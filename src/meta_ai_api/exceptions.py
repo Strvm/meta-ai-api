04abb1d2e636247a2e2077b61e667aeae3a654d5
Romeo Phillips <phillipsromeo@gmail.com>
2024-04-21 14:03:24 -0700

    feat: FB auth + image generation

---
+++ src/meta_ai_api/exceptions.py
@@ -0,0 +1,2 @@
+class FacebookInvalidCredentialsException(Exception):
+    pass
--- src/meta_ai_api/utils.py
+++ src/meta_ai_api/utils.py
@@ -1,5 +1,12 @@
+import logging
 import random
 import time
+
+from requests_html import HTMLSession
+import requests
+from bs4 import BeautifulSoup
+
+from meta_ai_api.exceptions import FacebookInvalidCredentialsException
 
 
 def generate_offline_threading_id() -> str:
@@ -71,3 +78,178 @@ def format_response(response: dict) -> str:
     ):
         text += content["text"] + "\n"
     return text
+
+
+# Function to perform the login
+def get_fb_session(email, password):
+    login_url = "https://mbasic.facebook.com/login/"
+    headers = {
+        "authority": "mbasic.facebook.com",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-US,en;q=0.9",
+        "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    }
+    # Send the GET request
+    response = requests.get(login_url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Parse necessary parameters from the login form
+    lsd = soup.find("input", {"name": "lsd"})["value"]
+    jazoest = soup.find("input", {"name": "jazoest"})["value"]
+    li = soup.find("input", {"name": "li"})["value"]
+    m_ts = soup.find("input", {"name": "m_ts"})["value"]
+
+    # Define the URL and body for the POST request to submit the login form
+    post_url = "https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100"
+    data = {
+        "lsd": lsd,
+        "jazoest": jazoest,
+        "m_ts": m_ts,
+        "li": li,
+        "try_number": "0",
+        "unrecognized_tries": "0",
+        "email": email,
+        "pass": password,
+        "login": "Log In",
+        "bi_xrwh": "0",
+    }
+
+    headers = {
+        "authority": "mbasic.facebook.com",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "content-type": "application/x-www-form-urlencoded",
+        "cookie": f"datr={response.cookies.get('datr')}; sb={response.cookies.get('sb')}; ps_n=1; ps_l=1",
+        "dpr": "2",
+        "origin": "https://mbasic.facebook.com",
+        "pragma": "no-cache",
+        "referer": "https://mbasic.facebook.com/login/",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "viewport-width": "1728",
+    }
+
+    # Send the POST request
+    session = requests.session()
+
+    result = session.post(post_url, headers=headers, data=data)
+    cookies = {
+        **result.cookies.get_dict(),
+        "sb": session.cookies["sb"],
+        "xs": session.cookies["xs"],
+        "fr": session.cookies["fr"],
+        "c_user": session.cookies["c_user"],
+    }
+
+    response_login = {
+        "cookies": cookies,
+        "headers": result.headers,
+        "response": response.text,
+    }
+    meta_ai_cookies = get_cookies()
+
+    url = "https://www.meta.ai/state/"
+
+    payload = f'__a=1&lsd={meta_ai_cookies["lsd"]}'
+    headers = {
+        "authority": "www.meta.ai",
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "content-type": "application/x-www-form-urlencoded",
+        "cookie": f'ps_n=1; ps_l=1; dpr=2; _js_datr={meta_ai_cookies["_js_datr"]}; abra_csrf={meta_ai_cookies["abra_csrf"]}; datr={meta_ai_cookies["datr"]};; ps_l=1; ps_n=1',
+        "origin": "https://www.meta.ai",
+        "pragma": "no-cache",
+        "referer": "https://www.meta.ai/",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    state = extract_value(response.text, start_str='"state":"', end_str='"')
+
+    url = f"https://www.facebook.com/oidc/?app_id=1358015658191005&scope=openid%20linking&response_type=code&redirect_uri=https%3A%2F%2Fwww.meta.ai%2Fauth%2F&no_universal_links=1&deoia=1&state={state}"
+    payload = {}
+    headers = {
+        "authority": "www.facebook.com",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "cookie": f"datr={response_login['cookies']['datr']}; sb={response_login['cookies']['sb']}; c_user={response_login['cookies']['c_user']}; xs={response_login['cookies']['xs']}; fr={response_login['cookies']['fr']}; m_page_voice={response_login['cookies']['m_page_voice']}; abra_csrf={meta_ai_cookies['abra_csrf']};",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "cross-site",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    }
+    session = requests.session()
+    response = session.get(url, headers=headers, data=payload, allow_redirects=False)
+
+    next_url = response.headers["Location"]
+
+    url = next_url
+
+    payload = {}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Referer": "https://www.meta.ai/",
+        "Connection": "keep-alive",
+        "Cookie": f'dpr=2; abra_csrf={meta_ai_cookies["abra_csrf"]}; datr={meta_ai_cookies["_js_datr"]}',
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "cross-site",
+        "Sec-Fetch-User": "?1",
+        "TE": "trailers",
+    }
+    session.get(url, headers=headers, data=payload)
+    cookies = session.cookies.get_dict()
+    if "abra_sess" not in cookies:
+        raise FacebookInvalidCredentialsException(
+            "Was not able to login to Facebook. Please check your credentials. "
+            "You may also have been rate limited. Try to connect to Facebook manually."
+        )
+    logging.info("Successfully logged in to Facebook.")
+    return cookies
+
+
+def get_cookies() -> dict:
+    """
+    Extracts necessary cookies from the Meta AI main page.
+
+    Returns:
+        dict: A dictionary containing essential cookies.
+    """
+    session = HTMLSession()
+    response = session.get("https://www.meta.ai/")
+    return {
+        "_js_datr": extract_value(
+            response.text, start_str='_js_datr":{"value":"', end_str='",'
+        ),
+        "abra_csrf": extract_value(
+            response.text, start_str='abra_csrf":{"value":"', end_str='",'
+        ),
+        "datr": extract_value(
+            response.text, start_str='datr":{"value":"', end_str='",'
+        ),
+        "lsd": extract_value(
+            response.text, start_str='"LSD",[],{"token":"', end_str='"}'
+        ),
+    }
